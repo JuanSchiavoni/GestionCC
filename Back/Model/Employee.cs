78ad743a4b3fbbd7c4fc724343e0e91a6206d386
Juan <juanschiavoni713@gmail.com>
2024-11-01 14:00:51 -0300

    Agregar archivos de proyecto.

---
+++ Back/Model/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Back.Model
+{
+    internal class Employee : Person
+    {
+        public string Role = string.Empty;
+    }
+}
---
+++ Back/Model/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Back.Model
+{
+    internal class Person
+    {
+        public long Id = 0;
+        public string Name = string.Empty;
+        public string Lastname = string.Empty;
+        public string Email = string.Empty;
+
+    }
+}
